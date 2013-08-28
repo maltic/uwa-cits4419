f9ed2634bef5bad480e96766485d2be3026495a9
Ash Tyndall <github@atyndall.net>
2013-08-28 15:05:04 +0800

    Update network.py

--- python/network.py
+++ python/network.py
@@ -1,1 +1,16 @@
-a
+import sockets
+
+input_buffer = []
+
+def send(msg, recipient):
+  # Writes to UDP socket?
+  return
+
+def recieve():
+  _read_socket()
+  ret = input_buffer
+  input_buffer = []
+  return ret
+  
+def _read_socket():
+  # Store messages in buffer
