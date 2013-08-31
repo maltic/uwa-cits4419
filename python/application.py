732063511d77be75dc8f7358c183342ace8ef0e6
huili <hleow13@gmail.com>
2013-08-31 15:12:16 +0800

    application

--- python/application.py
+++ python/application.py
@@ -1,3 +1,16 @@
-import dsr
+#import dsr
 
-# Your chat application here
+class application:
+    def __init__(self):
+        self.buffer = {}
+        
+    def send(self):
+        msg = input("Message: ")
+        toId = input("To: ")
+        dsr.send(msg, toId)
+
+    def receive(self):
+        for sender in self.buffer:
+            print(self.buffer[sender])
+        self.buffer = {}
+        
