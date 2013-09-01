1c8284bcda559d1cf0dd1bef19d434e0de649d72
tardoe <raphael.timothy@gmail.com>
2013-09-01 23:32:53 +0800

    Threaded server component untested with a similar client.

--- python/network.py
+++ python/network.py
@@ -1,16 +1,70 @@
-import sockets
+import socket
+import select
+import sys
+import thread
+
+# Client and server components rolled into one
+# Server is started on a new thread and each of up to 10 concurrent connections are dealt with on their own threads.
 
 input_buffer = []
+host = ''
+port = 6969
+recv_buffer = 1024
 
-def send(msg, recipient):
-  # Writes to UDP socket?
-  return
+def send(msg, addr):
+	# writes max 1024 byte to the remote address
+	
+	sock = socket.socket()
+	
+	try:
+		sock.connect(addr, port)
+	except Exception, e:
+		alert("didn't connect to server")
+	
+	sock.send(msg)
+	
+	sock.close()
+	
+	return
 
 def recieve():
-  _read_socket()
-  ret = input_buffer
-  input_buffer = []
-  return ret
+	ret = input_buffer
+	input_buffer = []
+	return ret
+
   
-def _read_socket():
-  # Store messages in buffer
+ def _init_server(host, port):
+	
+	sock = socket(AF_INET, SOCK_DGRAM)
+	sock.bind(host)
+	sock.listen(5)
+ 
+	while 1
+		clientsock, addr = sock.accept()
+		thread.start_new_thread(_server_handler, (clientsock, addr))
+	
+	
+	return
+  
+ def _start_server():
+ 
+	thread.start_new_thread(_init_server, (host, port))
+	
+	
+	return
+
+ def _server_handler(clientsock, addr):
+ 
+	#receive some bytes
+	while 1
+		data = clientsock.recv(recv_buffer)
+		if not data: break
+	
+	#append them to the buffer
+	input_buffer.append(data)
+	
+	#end the client socket connection
+	clientsock.close()
+
+	return
+	
