c5faa3dd6d7bdbab838d395ae4c0a0291effdfbe
Tim Raphael <raphael.timothy@gmail.com>
2013-09-03 22:09:37 +0800

    Swapped to a better server implementation and used forking instead of threading to background the server component

--- python/network.py
+++ python/network.py
@@ -1,70 +1,73 @@
 import socket
-import select
-import sys
-import thread
+import threading
+import SocketServer
 
 # Client and server components rolled into one
 # Server is started on a new thread and each of up to 10 concurrent connections are dealt with on their own threads.
 
 input_buffer = []
-host = ''
-port = 6969
-recv_buffer = 1024
+HOST = "localhost"
+UDP_PORT = 6969
+RECV_BUFFER = 1024
 
+#Subclass the base handler and add our functionality.
+class ReceiveHandler(SocketServer.BaseRequestHandler):
+
+	def handle(self):
+	
+		data = self.request[0].strip()
+		clientsock = self.request[1]
+		
+		input_buffer.append(data)
+		
+		print "Received data from {}".format(self.client_address[0])
+		print data
+	
+	#end def
+
+#end class	
+
+#Sub-class the UDP server and enable threading
+class ServerThreaded(SocketServer.ForkingMixIn, SocketServer.UDPServer):
+	pass
+		
 def send(msg, addr):
 	# writes max 1024 byte to the remote address
 	
-	sock = socket.socket()
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+	sock.sendto(msg, addr)
 	
-	try:
-		sock.connect(addr, port)
-	except Exception, e:
-		alert("didn't connect to server")
-	
-	sock.send(msg)
-	
+	print "sent message", msg
+
 	sock.close()
 	
 	return
+	
+#end def
 
 def receive():
 	ret = input_buffer
 	input_buffer = []
+	
 	return ret
+	
+#end def
 
-  
- def _init_server(host, port):
 	
-	sock = socket(AF_INET, SOCK_DGRAM)
-	sock.bind(host)
-	sock.listen(5)
- 
-	while 1
-		clientsock, addr = sock.accept()
-		thread.start_new_thread(_server_handler, (clientsock, addr))
+# How to setup the server:
+server = ServerThreaded((HOST, UDP_PORT), ReceiveHandler)
+ip, port  = server.server_address
 	
+#Start server on it's own thread
+#it'll spawn a new thread for each request
 	
-	return
-  
- def _start_server():
- 
-	thread.start_new_thread(_init_server, (host, port))
-	
-	
-	return
+server_thread = threading.Thread(target=server.serve_forever)
+server_thread.start()
+print "Server Started..."
 
- def _server_handler(clientsock, addr):
- 
-	#receive some bytes
-	while 1
-		data = clientsock.recv(recv_buffer)
-		if not data: break
-	
-	#append them to the buffer
-	input_buffer.append(data)
-	
-	#end the client socket connection
-	clientsock.close()
 
-	return
-	
+# How to use the 'Send()' function
+send("message 1", ("localhost", 6969))
+send("message 2", ("localhost", 6969))
+
