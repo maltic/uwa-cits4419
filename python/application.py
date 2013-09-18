351f5b4d6c923b53b441a48bfd79002b89a873e2
Ash Tyndall <atyndall@ucc.gu.uwa.edu.au>
2013-09-18 15:03:01 +0800

    Refactor application

--- python/application.py
+++ python/application.py
@@ -1,16 +1,16 @@
 import dsr
 
-class application:
-    def __init__(self):
-        self.buffer = []
+class Application:
+  def __init__(self):
+    self.buffer = []
         
-    def send(self):
-        msg = input("Message: ")
-        toId = input("To: ")
-        dsr.send(msg, toId)
+  def send(self):
+    msg = input("Message: ")
+    toId = input("To: ")
+    dsr.send(msg, toId)
 
-    def receive(self):
-        for (msg, senderId) in self.buffer:
-            print(senderId+':', msg)
-        self.buffer = []
+  def receive(self):
+    for (msg, senderId) in self.buffer:
+      print(senderId+':', msg)
+      self.buffer = []
         
