48caad15480cb325f6d8d5b1911ff786b4a52fe1
Ash Tyndall <github@atyndall.net>
2013-10-16 15:53:47 +0800

    Add graceful termination

--- python/simulator.py
+++ python/simulator.py
@@ -35,16 +35,18 @@ CAN_TALK = [(0,  [ [0, 1, 1, 1, 1],    # 0
            # 1 = True
 
 NUM_NODES = 5
+RUN_LOOPS = 10
 
 COMM_LOC = 'logs/'
 
-def _node_simulation(q, log_to, node_addr, other_nodes):
+def _node_simulation(q, log_to, node_addr, other_nodes, loops):
   sys.stdout = open(log_to, 'a')
   print('SIMULATOR: Node {} online'.format(node_addr))
 
   d = dsr.DSR(q, node_addr)
 
-  while True:
+  for i in range(loops):
+    print('== LOOP {} =='.format(i))
     to = random.choice(other_nodes)
     contents = ''.join([random.choice(string.ascii_letters) for _ in range(6)])
 
@@ -62,29 +64,34 @@ def _node_simulation(q, log_to, node_addr, other_nodes):
     sys.stdout.flush()
     sys.stderr.flush()
 
+  d.network.stop_timer()
+  print('==== END ====')
+
 class Simulator:
-  def __init__(self, num_nodes, talk_matrix):
+  def __init__(self, num_nodes, talk_matrix, loops, comm_loc):
     #assert(len(talk_matrix) == num_nodes)
 
     self.num_nodes = num_nodes
-    self.talk_matrix = talk_matrix
+    #self.talk_matrix = [ [bool(x) for x in y] for y in talk_matrix ]
     self.processes = [None] * num_nodes
     self.out_pipes = [None] * num_nodes
     self.in_pipes = [None] * num_nodes
+    self.comm_loc = comm_loc
+    self.loops = loops
     self.start_time = 0
 
     for i in range(num_nodes):
       out_p, in_c = Pipe()
       in_p, out_c = Pipe()
-      args = ((out_c, in_c), COMM_LOC + str(i) + '.log', i, [x for x in range(num_nodes) if x != i])
+      args = ((out_c, in_c), self.comm_loc + str(i) + '.log', i, [x for x in range(num_nodes) if x != i], loops)
       self.processes[i] = Process(target=_node_simulation, args=args)
       self.out_pipes[i] = out_p
       self.in_pipes[i] = in_p
 
   def can_talk(self, a, b):
     time_diff = int(time.time() - self.start_time)
-    prev_m = self.talk_matrix[0][1]
-    for t, m in self.talk_matrix:
+    prev_m = CAN_TALK[0][1]
+    for t, m in CAN_TALK:
        if time_diff > t:
           prev_m = m
        if time_diff < t:
@@ -93,18 +100,22 @@ class Simulator:
     return prev_m[a][b] == 1
 
   def start(self):
+    print('Running simulation; will terminate automatically and gracefully after {} loops.'.format(self.loops))
+	
     [p.start() for p in self.processes]
     self.start_time = time.time()
 
-    while True:
+    while any([p.is_alive() for p in self.processes]):
       for i, pipe in enumerate(self.in_pipes):
         while pipe.poll():
           toaddr, msg = pipe.recv()
           for j, opipe in enumerate(self.out_pipes):
             if self.can_talk(i, j):
               opipe.send((i, msg))
+			  
+    print('Completed. Consult logs at "{}" for more info'.format(self.comm_loc))
 
 if __name__ == '__main__':
   freeze_support()
-  s = Simulator(NUM_NODES, CAN_TALK)
+  s = Simulator(NUM_NODES, CAN_TALK, RUN_LOOPS, COMM_LOC)
   s.start()
