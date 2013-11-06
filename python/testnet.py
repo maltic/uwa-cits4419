b23f98566840f2426d7d5e6a01d9323c5e27e62b
Bowie Chung <bcbc66822@yahoo.com>
2013-11-06 19:30:49 +0800

    Added changes

--- python/testnet.py
+++ python/testnet.py
@@ -58,28 +58,36 @@ import dsr
 
 class TestNet:
   def __init__(self):
-    self.dsr1 = dsr.DSR(self, 1)
-    self.dsr2 = dsr.DSR(self, 2)
-    self.dsr3 = dsr.DSR(self, 3)
+    self.dsr1 = dsr.DSR(1)
+    self.dsr2 = dsr.DSR(2)
+    self.dsr3 = dsr.DSR(3)
   def send(self, msg, addr):
     self.dsr1.receive_packet(msg)
     self.dsr2.receive_packet(msg)
     self.dsr3.receive_packet(msg)
   def runSim(self):
-    self.dsr1.send_message("test packet", 2)
+    self.dsr1.send_message("test packet weeeeeeeee", 2)
     while True:
+      tmp = self.dsr1.pop_outbox()
+      tmp.extend(self.dsr2.pop_outbox())
+      tmp.extend(self.dsr3.pop_outbox())
+      for p in tmp:
+        self.send(p[0],p[1])
       self.dsr1.update()
       self.dsr2.update()
       self.dsr3.update()
-      msgs = self.dsr2.pop_messages()
+      msgs = self.dsr1.pop_inbox()
+      msgs.extend(self.dsr2.pop_inbox())
+      msgs.extend(self.dsr3.pop_inbox())
       if msgs != []:
         print("Messages receied by dsr2 : {} path was {}".format(msgs[0].contents, msgs[0].path))
-        
+        return
+
 if __name__ == '__main__':
   tn = TestNet()
   tn.runSim()
-      
-     
-    
-  
-  
+
+
+
+
+
