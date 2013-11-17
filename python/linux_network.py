766f0dcb42eefa48bfe38618599d3029cd91537c
Tim Raphael <raphael.timothy@gmail.com>
2013-11-18 07:58:15 +0800

    Linux Networking is now working with correct DSR processing

--- python/linux_network.py
+++ python/linux_network.py
@@ -1,31 +1,30 @@
-input_buffer = []
-LOG = True
-import Packet
 import sys
 import dsr
-#from scapy.all import *
-import socketserver
 import socket
 import threading
+import time
 
 
 #Sub-class the UDP server and enable threading
-class ServerThreaded(socketserver.ForkingMixIn, socketserver.UDPServer):
-	pass
+#class ServerThreaded(socketserver.ForkingMixIn, socketserver.UDPServer):
+#	pass
 
 #Subclass the base handler and add our functionality.
-class ReceiveHandler(socketserver.BaseRequestHandler):
-	
-	def handle(self):
-		data = self.request[0].strip()
-		pkt = CustPacket.CustPacket(data)
-		
-		#extract out DSR string
-		dsr_data = pkt.udp_data
-		
-		global input_buffer
-		
-		input_buffer.append(dsr_data)
+#class ReceiveHandler(socketserver.BaseRequestHandler):
+#	
+#	def handle(self):
+#		data = self.request[0].strip()
+#		pkt = CustPacket.CustPacket(data)
+#		
+#		#extract out DSR string
+#		dsr_data = pkt.udp_data
+#		
+#		global input_buffer
+#		
+#		input_buffer.append(dsr_data)
+#
+#		print("recieved something!!")
+#
 #end def
 
 #end class
@@ -35,72 +34,108 @@ class Network:
 
 		#self.wireless_interface = wlan_int
 		self.ip_address = ip_address
+		self.dsr_port = dsr_port
 
 		tokens = ip_address.split(".")
 		self.id = tokens[3]
 		self.net_prefix = tokens[0] + "." + tokens[1] + "." + tokens[2] + "."
 		
+		self.send_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.send_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+		self.send_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		
 		#setup sending socket
-		self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+		self.input_buffer = []
 
-		server = ServerThreaded((ip_address, dsr_port), ReceiveHandler)
-		ip, port  = server.server_address
-		server_thread = threading.Thread(target=server.serve_forever)
-		server_thread.start()
+		self.server_thread = threading.Thread(target=self.serve_socket)
+		self.server_thread.start()
+	
+#		server = ServerThreaded((ip_address, dsr_port), ReceiveHandler)
+#		ip, port  = server.server_address
+#		server_thread = threading.Thread(target=server.serve_forever)
+#		server_thread.start()
 
 	def receive(self) :
-		global input_buffer
-		ret = input_buffer
-		input_buffer = []
+		ret = self.input_buffer
+		self.input_buffer = []
+		return ret
 
 	def send(self, msg, addr) :
-		if addr == 255 :
-			dsr_add = "255.255.255.255"
+		if addr == -1 :
+		#			dst_addr = self.net_prefix + "255"
+			dst_addr = "255.255.255.255"
 		else :
-			dst_addr = self.net_prefix + addr
+			dst_addr = self.net_prefix + str(addr)
 
 		#pkt = IP(dst=dst_addr)/UDP(dport=self.dsr_port, sport=RandNum(1024,65535))/msg
 
 		#send(pkt)
-		self.socket.sendto(msg, (dst_addr, self.dsr_port))
+		print("NET: sending '" + str(msg) + "' to " + str(dst_addr))
+		
+		
+		self.send_socket.sendto(bytes(msg,'UTF-8'), (dst_addr, self.dsr_port))
 
+	def serve_socket(self):
 
+		print("Starting socket server...")
+		self.recv_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.recv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+		self.recv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		
+		self.recv_socket.bind(('0.0.0.0', self.dsr_port))
+
+		while True:
+			message, address = self.recv_socket.recvfrom(1024)
+			
+			if address[0] != self.ip_address:
+					self.input_buffer.append(message.decode(encoding="UTF-8"))
 #main loop
 
-dsr = dsr.DSR(1)
-network = Network("10.211.55.4", 1069)
-msgNumber = 1
-loop_number = 0
+arg_address = sys.argv[1]
+node_id = arg_address[-1:]
+
+dsr = dsr.DSR(int(node_id))
+
+network = Network(arg_address, 1069)
 
 while True:
 	
-	if loop_number == 50000:
-		user_msg = "hello" + str(msgNumber)
-		msgNumber += 1
+	user_msg = "hello from " + str(node_id)
 	
+	if node_id == "1":
+		user_dst = 2
+	else:
 		user_dst = 1
 	
-		print("Sending message '" + user_msg + "' to dst: " + str(user_dst))
+	print("Sending message '" + user_msg + "' to dst: " + str(user_dst))
 
-		dsr.send_message(user_msg, user_dst)
-
-		loop_number = 0
-
-	else :
-		loop_number = loop_number + 1
+	dsr.send_message(user_msg, user_dst)
 
 	dsr.update()
-	
-	input = network.receive()
+	dsr.update()
+	dsr.update()
 
-	if input != None:
-		
-		print("got some shiz from the network" + str(input))
-		
-		for m in input:
-			full_packet = Packet(m)
-			dsr.receive_packet(full_packet.udp_data)
+	dsrOutbox = dsr.pop_outbox()
+
+	if dsrOutbox != []:
+		print("DSR Outbox: " + str(dsrOutbox))
+
+	for o in dsrOutbox:
+		network.send(o[0], o[1])
+
+	networkInput = network.receive()
+
+	if networkInput != []:
+				
+		for m in networkInput:
+			print("Received Message: '" + str(m) + "'")
+			dsr.receive_packet(m)
+
+	dsr.update()
+	dsr.update()
+	dsr.update()
+
+	time.sleep(4)
 
 
 
