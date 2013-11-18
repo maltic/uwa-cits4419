4e1a83ae161b68278f10a201900162acb567f0f0
Ash Tyndall <github@atyndall.net>
2013-11-18 15:53:45 +0800

    Create pull.sh

---
+++ pull.sh
@@ -0,0 +1,20 @@
+# dsr.zip must contain the contents of the python directory
+
+# you can run a sample python webserver to serve up dsr.zip
+# using `python -m http.server 80`
+
+
+cd ~/Deployment
+rm dsr.zip
+
+wget "http://10.1.1.200/dsr.zip"\
+        && cd ~/DSR
+        && ./stop.sh\
+        && rm -rf ~/DSR\
+        && mkdir ~/DSR\
+        && unzip dsr.zip -d ~/DSR\
+        && chmod +x ~/DSR/stop.sh\
+        && chmod +x ~/DSR/start.sh\
+
+cd ~/DSR\
+&& ./start.sh >~/DSRlog/`date`.log 2>&1 &
